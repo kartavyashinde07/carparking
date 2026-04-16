24292c6ab9e0471bcd178dcaa53c678f9f3f87c0
kartavyashinde07 <Kartavyashinde07@gmail.com>
2026-04-16 17:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -840,7 +840,7 @@ def init_db():
 
 
 # ---------------- RUN ----------------
-if __name__ == "__main__":
-    with app.app_context():
+with app.app_context():
         init_db()
+if __name__ == "__main__":
     app.run(debug=True)
